f045102ac39d1af2478dd72e25449b3fd3dc7090
Akshat <helloakshatsengar@gmail.com>
2025-07-20 07:05:39 +0530

    updated everything

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={true} disableTransitionOnChange={false}>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={true} disableTransitionOnChange={false}>
           {children}
         </ThemeProvider>
       </body>
